285869477b67d234d674c60e7d03d3611cdb789c
Ademola Onasoga <ademola.onasoga@gmail.com>
2025-01-10 01:52:56 +0100

    add a loader and loading component

---
+++ src/app/dashboard/users/loading.tsx
@@ -0,0 +1,9 @@
+import Spinner from "@/components/common/spinner";
+
+export default function Loading() {
+  return (
+    <div className="flex w-100 justify-center">
+      <Spinner />
+    </div>
+  );
+}
