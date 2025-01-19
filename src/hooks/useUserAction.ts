b28cbbd6515ebe6af3d1d3fc326fe1700130287e
Ademola Onasoga <ademola.onasoga@gmail.com>
2025-01-19 15:53:10 +0100

    refactor: add useCallback to functions passed into children componments

--- src/hooks/useUserAction.ts
+++ src/hooks/useUserAction.ts
@@ -2,46 +2,46 @@ import { IFilterUserProps } from "@/interface/IFilterUser";
 import IUserResp from "@/interface/IUser";
 import { Action, status, type } from "@/interface/IUserReducer";
 import handleOrgFilter from "@/utils/handleOrgFilter";
-import { Dispatch } from "react";
+import { Dispatch, useCallback } from "react";
 
 export const useUserAction = (dispatch: Dispatch<Action>) => {
 
-    const handleAddUsers = (users: IUserResp[]) => {
+  const handleAddUsers = useCallback((users: IUserResp[]) => {
         dispatch({
           type: type.ADD_USER,
           payload: users
         })
-    };
+    }, [dispatch])
 
-    const handleFilterUsers = (users: IUserResp[] | null) => {
+    const handleFilterUsers = useCallback((users: IUserResp[] | null) => {
       dispatch({
         type: type.FILTER_USER,
         payload: users
       })
-    };
+    }, [dispatch])
   
-    const handleUpdateCurrentPage = (pageNumber: number) => {
+    const handleUpdateCurrentPage = useCallback((pageNumber: number) => {
       dispatch({
         type: type.UPDATE_CURRENT_PAGE_USER,
         payload: pageNumber
     })
-    };
+    }, [dispatch])
   
-    const handleUpdateStatus = (state: string, status: boolean) => {
+    const handleUpdateStatus = useCallback((state: string, status: boolean) => {
       dispatch({
         type: type.UPDATE_STATUS,
         payload: { status, state }
         })
-    };
+    }, [dispatch]);
 
-    const handleUpdateRow = (rowNumber: number) => {
+    const handleUpdateRow = useCallback((rowNumber: number) => {
       dispatch({
         type: type.UPDATE_ROW_NUMBER,
         payload: rowNumber,
       })
-    }
+    }, [dispatch])
 
-    const handleFilter = (
+    const handleFilter = useCallback((
       filterCriteria: IFilterUserProps | undefined, 
       users?: IUserResp[]
     ) => {
@@ -57,7 +57,7 @@ export const useUserAction = (dispatch: Dispatch<Action>) => {
         const filtUsers = handleOrgFilter(users, filterCriteria)
         return handleFilterUsers(filtUsers)
       }
-  }
+  }, [handleFilterUsers, handleUpdateStatus])
 
     return { handleAddUsers, handleUpdateCurrentPage, handleUpdateStatus, handleUpdateRow, handleFilter }
   
